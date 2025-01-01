e92512f48ed51d795a5a1546a4e36cf2bb42dc50
Cory Suzuki <131649685+CorySuzuki1729@users.noreply.github.com>
2024-12-31 23:13:29 -0800

    LMS Sign Sign Algorithm

---
+++ aalmssignsign.m
@@ -0,0 +1,15 @@
+function[w,y,e,J,w1] = aalmssignsign(x,dn,mu,M)
+    %x = input data to the filter;
+    %M = order of the filter;
+    %mu = step size parameter;
+    N = length(x);
+    y = zeros(1,N);
+    w = zeros(1,M);
+    for n = M:N
+        x1 = x(n:-1:n-M+1);
+        y(n) = w*x1';
+        e(n) = dn(n) - y(n);
+        w = w+2*mu*sign(e(n))*sign(x1);
+        w1(n-M+1,:) = w(1,:);
+    end;
+    J = e.^2;
